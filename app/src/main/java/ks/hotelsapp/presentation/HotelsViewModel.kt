f5bbeae8b57ef4461ea56eba0b943a2f93cc557c
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-01-28 17:21:30 +0300

    was added progress bar

--- app/src/main/java/ks/hotelsapp/presentation/HotelsViewModel.kt
+++ app/src/main/java/ks/hotelsapp/presentation/HotelsViewModel.kt
@@ -13,18 +13,25 @@ import javax.inject.Inject
 @HiltViewModel
 class HotelsViewModel @Inject constructor(
     // private val repository: HotelsRepositoryImpl,
-    private val getHotelsUseCase: GetHotelsUseCase// getHotelsUseCase: GetHotelsUseCase
+    private val getHotelsUseCase: GetHotelsUseCase
 ) : ViewModel() {
 
     private val _hotels = MutableLiveData<List<Hotel>>()
     val hotels: LiveData<List<Hotel>> = _hotels
 
+    // LiveData для отслеживания состояния загрузки
+    private val _isLoading = MutableLiveData<Boolean>(false)
+    val isLoading: LiveData<Boolean> = _isLoading
+
     fun loadHotels() {
+        _isLoading.value = true // Включаем индикатор загрузки
         viewModelScope.launch {
             try {
                 _hotels.value = getHotelsUseCase()
             } catch (e: Exception) {
                 // Handle error
+            } finally {
+                _isLoading.value = false // Отключаем индикатор загрузки
             }
         }
     }
--- app/src/main/java/ks/hotelsapp/presentation/ListOfHotelsFragment.kt
+++ app/src/main/java/ks/hotelsapp/presentation/ListOfHotelsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -29,6 +30,13 @@ class ListOfHotelsFragment : Fragment() {
 
         val recyclerView: RecyclerView = view.findViewById(R.id.rcView)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val progressBar: ProgressBar = view.findViewById(R.id.progressBar) // ProgressBar для загрузки
+
+        // Наблюдаем за загрузкой
+        viewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
+            progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+        }
 
         viewModel.hotels.observe(viewLifecycleOwner) { hotels ->
             recyclerView.adapter = HotelsAdapter(hotels)
