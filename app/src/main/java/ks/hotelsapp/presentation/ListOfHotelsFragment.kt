c8db845a8923e89b5aaa70868b7570ef174a342b
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-01-28 13:00:36 +0300

    place for add image and logic for it

--- app/src/main/java/ks/hotelsapp/presentation/ListOfHotelsFragment.kt
+++ app/src/main/java/ks/hotelsapp/presentation/ListOfHotelsFragment.kt
@@ -52,37 +52,3 @@ class ListOfHotelsFragment : Fragment() {
     }
 }
 
-
-//class ListOfHotelsFragment : Fragment() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        return inflater.inflate(R.layout.fragment_list_of_hotels, container, false)
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        val recyclerView: RecyclerView = view.findViewById(R.id.rcView)
-//        val testData = listOf(
-//            Hotel(123, "Отель" , "Broadway, 10", 4.75f,"500 м", 3),
-//            Hotel(123, "Отель" , "Broadway, 10", 4.75f,"500 м", 3),
-//            Hotel(123, "Отель" , "Broadway, 10", 4.75f,"500 м", 3),
-//            Hotel(123, "Отель" , "Broadway, 10", 4.75f,"500 м", 3),
-//            Hotel(123, "Отель" , "Broadway, 10", 4.75f,"500 м", 3),
-//            Hotel(123, "Отель" , "Broadway, 10", 4.75f,"500 м", 3),
-//
-//        )
-//
-//        val adapter = HotelsAdapter(testData)
-//        recyclerView.adapter = adapter
-//        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-//    }
-//
-//}
