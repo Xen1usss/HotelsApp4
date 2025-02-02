6648e0241b27d21fa591f2a3a0a68a981c2806a2
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-02-03 00:22:31 +0300

    test for new changes

--- app/src/main/java/ks/hotelsapp/presentation/HotelDetailsComposeFragment.kt
+++ app/src/main/java/ks/hotelsapp/presentation/HotelDetailsComposeFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -32,7 +31,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
