272d704a2763f306546f04ebadd8c48cb2d303f4
Ksenia <130775320+Xen1usss@users.noreply.github.com>
2025-02-02 18:34:39 +0300

    add rating

--- app/src/main/java/ks/hotelsapp/presentation/HotelDetailsComposeFragment.kt
+++ app/src/main/java/ks/hotelsapp/presentation/HotelDetailsComposeFragment.kt
@@ -7,25 +7,18 @@ import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,7 +44,8 @@ fun ComposeScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
         Image(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(16.dp),
             painter = painterResource(R.drawable.test),
             contentDescription = null,
             contentScale = ContentScale.FillWidth
@@ -64,6 +58,16 @@ fun ComposeScreen() {
                 text = "Best Western President Hotel at Times Square",
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(top = 10.dp),
+                text = "Рейтинг: 3.4",
+                fontSize = 26.sp,
                 textAlign = TextAlign.Center
             )
         }
